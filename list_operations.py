49031547684915a1ebaa713ae0f3f559ca2f3f98
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-05 12:01:58 -0700

    Finished all Functions

--- list_operations.py
+++ list_operations.py
@@ -84,7 +84,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -97,7 +97,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -110,7 +110,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -123,7 +123,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -139,7 +139,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -153,8 +153,8 @@ def replace_head(input_list):
     True
 
     """
+    input_list[0] = 42
 
-    pass
 
 
 def replace_third_and_last(input_list):
@@ -169,8 +169,8 @@ def replace_third_and_last(input_list):
 
     """
 
-    pass
-
+    input_list[2] = 37
+    input_list[-1] = 37
 
 def replace_middle(input_list):
     """Replace all elements of a list but the first and last two with 42 and 37.
@@ -187,8 +187,8 @@ def replace_middle(input_list):
     True
 
     """
+    input_list[2:-2] = [42, 37]
 
-    pass
 
 
 def delete_third_and_seventh(input_list):
@@ -203,7 +203,8 @@ def delete_third_and_seventh(input_list):
 
     """
 
-    pass
+    input_list.pop(6)
+    input_list.pop(2)
 
 
 def delete_middle(input_list):
@@ -219,8 +220,8 @@ def delete_middle(input_list):
     True
 
     """
+    del input_list[2:-2]
 
-    pass
 
 
 ##############################################################################
